a42f74d89501cb3cc19a97b028f0023088c27bee
Pedro Akira <pedro-akira@hotmail.com>
2023-03-30 18:25:58 -0300

    move some files to the right dir

--- ironhack_css_clones/amazon/js/script.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-  $('#nav-search-select').change(function() {
-    var selectedText = $(this).find('option:selected').text();
-    $('#nav-search').find('.nav-search-label').html(selectedText);
-  });
-});
